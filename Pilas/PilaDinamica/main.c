6771b878652ef0e7f5946bc1cae3296c38a1eb03
Camila Rocio Serafini <camiiserafinii@gmail.com>
2025-01-06 21:31:10 -0300

    first commit

---
+++ Pilas/PilaDinamica/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "PilaDinámica.h"
+
+int main() {
+    TPila P;
+    TElementoP bit;
+    int num, numero;
+    IniciaP(&P);
+    printf("Ingrese un numero entero para calcular su binario ");
+    scanf("%d",&numero);
+    num=numero;
+    while (numero != 0) {
+        poneP(&P, numero % 2);
+        numero /= 2;
+    }
+    printf("Representacion de %d en base 2 \n", num);
+    while (!VaciaP(P)) {
+        sacaP(&P,&bit);
+        printf("%d ",bit);
+    }
+    return 0;
+}
---
+++ Pilas/PilaEstatica/PilaEstática.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "PilaEstática.h"
+
+void IniciaP(TPila *P)
+{
+    P->tope=-1;
+}
+
+void poneP(TPila *P, TElementoP x)
+{
+    if((P->tope) != MAX-1)
+        P->datos[++(P->tope)]=x;
+}
+
+void sacaP(TPila *P, TElementoP *x)
+{
+    if((P->tope)!= -1)
+    {
+        *x = P->datos[(P->tope)];  //es lo mismo que hacer *x = P->datos[(P->tope)--];
+        (P->tope)--;
+    }
+}
+
+TElementoP consultaP(TPila P)
+{
+    if((P.tope)!= -1)
+        return P.datos[P.tope];
+}
+
+int VaciaP(TPila P)
+{
+    return (P.tope == -1);
+}
+
+
+
